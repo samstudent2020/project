a8dacdbf112e030b6aeccf08cbde423cbddde28c
Dhruv Sandoval <dhruvsandoval@gmail.com>
2021-02-20 23:45:43 +0000

    This is a test

--- testProject/src/testProject/Movies.java
+++ testProject/src/testProject/Movies.java
@@ -8,6 +8,7 @@ public class Movies {
 	public static void main(String[] args)
 	{
 		System.out.println("This is a test to test if I'm able to commit new files under this repository");
+		System.out.print("This is a second test");
 	}
 	
 }
